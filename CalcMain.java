cc786e254cea9bf79c8f0b610b0eda1f0956e541
Cola <36576017+colaolala@users.noreply.github.com>
2018-02-24 22:04:06 +0300

    Add files via upload

---
+++ CalcMain.java
@@ -0,0 +1,8 @@
+import java.awt.event.ActionEvent;
+
+public class CalcMain {
+    public static void main (String args []) {
+        CalcEngine startGUI = new CalcEngine();
+        startGUI.makeGui();
+    }
+}
